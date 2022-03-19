4a7bc7f3a64549b8c1e627c7bc01b0a150ee521b
smilekison <smilekishan.sk@gmail.com>
2022-03-19 21:45:45 +0545

    Try this one

--- myApp.js
+++ myApp.js
@@ -1,46 +1,18 @@
-var express = require('express');
-var path = require('path');
+var express = require("express");
+var path = require("path");
 var app = express();
-app.use("/public", express.static( __dirname + "/public"))
 
-app.use("/json", (req, res)=>{return res.json({"message":"Hello json"})});
-
-app.get("/", (req, res)=>{
-  return res.sendFile(__dirname + "/views/index.html");
+let message = { message: "Hello json" };
+app.get("/json", (request, response) => {
+  if (process.env.MESSAGE_STYLE === "uppercase") {
+    response.json({ message: "HELLO JSON" });
+  } else {
+    response.json(message);
+  }
 });
 
+// app.get("/", (req, res) => {
+//   return res.sendFile(__dirname + "/views/index.html");
+// });
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- module.exports = app;
+module.exports = app;

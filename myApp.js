77262669bcadcf5f732fab0a82d0e7c9cee1305d
Smile Kisan <24309222+smilekison@users.noreply.github.com>
2022-03-19 08:10:51 +0000

    adding env file

--- myApp.js
+++ myApp.js
@@ -1,6 +1,13 @@
 var express = require('express');
+var path = require('path');
 var app = express();
+app.use("/public", express.static( __dirname + "/public"))
 
+app.use("/json", (req, res)=>{return res.json({"message":"Hello json"})});
+
+app.get("/", (req, res)=>{
+  return res.sendFile(__dirname + "/views/index.html");
+});
 
 
 
